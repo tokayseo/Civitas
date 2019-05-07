292a322871882da78a3012603751b8ca2faa57e8
Aleš Kalan <SloRunner@users.noreply.github.com>
2019-05-07 22:51:52 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt-add-repository -y ppa:bitcoin/bitcoin
+apt-get update
+apt-get upgrade
+apt-get -y install wget git unzip libevent-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libzmq3-dev nano libdb4.8-dev libdb4.8++-dev
+cd /var
+touch swap.img
+chmod 600 swap.img
+dd if=/dev/zero of=/var/swap.img bs=1024k count=6000
+mkswap /var/swap.img
+swapon /var/swap.img
+echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
